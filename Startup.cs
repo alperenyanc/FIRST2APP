c4cd9036839512688e11776211ffb43ac97f7ea0
Alp Eren YANCI <alperenyanci@outlook.com>
2019-05-29 17:38:48 +0300

    Added

--- Startup.cs
+++ Startup.cs
@@ -5,12 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace First2App
 {
     public class Startup
     {
+        public IConfiguration Configuration{get;}
+        public Startup(IConfiguration configuration){
+            Configuration=configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,14 +33,15 @@ namespace First2App
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+           /**  app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello heyoo World!");
-            });
+            });*/
+
             app.UseMvc(routes=>{
                 routes.MapRoute(
                 name:"default",
-                template:"{controller}/{action}/{id}"
+                template:"{controller=Home}/{action=Index}/{id?}"
 
                 );
             });
